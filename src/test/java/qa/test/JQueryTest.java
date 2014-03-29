272f715a6bea67ad056e4d99ecbe9585285906f7
Jon Austen <djangofan@gmail.com>
2014-03-29 10:47:28 -0700

    Still trying to get it working.  Almost got it.

--- src/test/java/qa/test/JQueryTest.java
+++ src/test/java/qa/test/JQueryTest.java
@@ -17,13 +17,13 @@ import com.google.common.io.Resources;
 public class JQueryTest {
 
 	// its nice to keep JavaScript snippets in separate files.
-	private final String JQUERY_SCRIPT = "jquery-1.11.0.min.js";
-	private final String JGROWL_SCRIPT = "jquery.jgrowl.js";
+	private final String JQUERY_SCRIPT = "jquery-1.9.0.js";
+	private final String JGROWL_SCRIPT = "jquery.jgrowl-1.2.13.js";
 
     @Test
 	public void runTest() {
 		WebDriver driver = new FirefoxDriver();
-		driver.get("about:home");
+		driver.get("http://djangofan.github.io/html-test-site/site/w1.html");
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
         URL jqueryUrl = Resources.getResource( JQUERY_SCRIPT );
@@ -36,32 +36,29 @@ public class JQueryTest {
 		} catch ( IOException ioe ) {
 			ioe.printStackTrace();
 		}
+		System.out.println("Executing '" + JQUERY_SCRIPT + "' to load within WebDriver instance...");
         js.executeScript( jqueryText );
+        System.out.println("Done loading jQuery.");
         js.executeScript( jgrowlText );
-
-        // loadCSS = function(href) { 
-        // 	  var cssLink = $("<link>");
-        // 	  $("head").append(cssLink); //IE hack: append before setting href 
-        // 	  cssLink.attr({
-        // 	    rel:  "stylesheet",
-        // 	    type: "text/css",
-        // 	    href: href
-        // 	  }); 
-        // 	};
+        
+        // try some basic javascript
+        System.out.println("Executing a Javascript command to verify everything is ok...");
+        js.executeScript( "document.getElementById( 'textFieldTestInputControlID' ).scrollIntoView(); " );
+        System.out.println("Done with Javascript command.");
         
         // type some text using Jquery to verify jquery works
-        js.executeScript( "$(\"#searchText\").text('Test message here');" );
-        
+        System.out.println("Executing a jquery click command to verify jQuery loaded...");
+        js.executeScript( "$(\"#textFieldTestInputControlID\").val(\"Test\");$(\"#textFieldTestProcessButtonID\").click();" );
+        System.out.println("Done with click command.");
 		
         // load jgrowl css using jquery
-        js.executeScript( "$(\"<link/>\", { rel: \"stylesheet\", type: \"text/css\", href: \"jquery.jgrowl.css\"}).appendTo(\"head\");" );
-        
+        //js.executeScript( "$(\"<link/>\", { rel: \"stylesheet\", type: \"text/css\", href: \"jquery.jgrowl.css\"}).appendTo(\"head\");" );
         
 		// test jgrowl
-		js.executeScript( "$.jGrowl(\"Sticky notification with a header.\", { header: 'A Header', sticky: true });" );
+		js.executeScript( "$.jGrowl(\"A message that will live a little longer.\", { life: 10000 }); " );
 		
         try {
-			Thread.sleep(10000);
+			Thread.sleep(30000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
